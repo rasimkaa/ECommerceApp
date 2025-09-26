bee7d5f5337aabc973e4fd375d5d1bcf4b7e81be
DESKTOP-U94P9TD\Admin <rasimcs2016@gmail.com>
2025-09-26 11:55:29 +0300

    Реализация логики: добавлены модель Product и ProductRepository

---
+++ ECommerceApp.Core/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Name} - {Price}₽";
+        }
+    }
+}
---
+++ ECommerceApp.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using ECommerceApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Data.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly List<Product> _products = new();
+
+        public void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _products;
+        }
+
+        public Product? GetById(int id)
+        {
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
